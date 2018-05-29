866727342faf44d26196b10a195c7a7d89a253a1
Anthony J. DeMeglio <22527727+ademeglio@users.noreply.github.com>
2018-05-29 19:09:15 -0400

    develop displayAccounts(); and refactor it

--- src/main/java/org/wecancodeit/bank_teller/BankingApp.java
+++ src/main/java/org/wecancodeit/bank_teller/BankingApp.java
@@ -21,13 +21,14 @@ public class BankingApp {
 	 */
 	
 	public static void main(String[] args) {
-		String account; 
+		String account = ""; 
 		double amount;
 		
 		Bank myBank = new Bank();
 		BankAccount account1 = new BankAccount("1111", "Checking", 500.00);
 		BankAccount account2 = new BankAccount("2222", "Savings", 2500.00);
 		Scanner userInput = new Scanner(System.in);
+	
 		
 		myBank.add(account1);
 		myBank.add(account2);
@@ -36,34 +37,73 @@ public class BankingApp {
 	
 		// Add accounts to the bank
 		while (true) {
-		// Display current account status
-			
-		// Menu (Loop)
-		System.out.println("Please make a transaction selection: ");
-		System.out.println("1. Deposit      	4. Close Account"
-					   + "\n2. Withrawal	 	5. EXIT"
-					   + "\n3. Check Balance");
 		
-		String response = userInput.nextLine();
-		
-		switch (response) {
-		case "1" : // Deposit
-			System.out.println("Deposit, please enter account number.");
-			account = userInput.nextLine();
-			System.out.println("Please enter your deposit amount:");
-			amount = userInput.nextDouble();
-			userInput.nextLine();
-			myBank.deposit(account, amount);
-			System.out.println("Account Balance is now $" 
-			+ myBank.findAccount(account).getBalance());
-			break;
-		}
+			// Display current account status
+			displayAccounts(collection);	
+
+			// Menu (Loop)
+			System.out.println("\nPlease make a transaction selection: ");
+			System.out.println("1. Deposit      	4. Close Account"
+						   + "\n2. Withrawal	 	5. EXIT"
+						   + "\n3. Check Balance");
 			
+			String response = userInput.nextLine();
+			
+			switch (response) {
+			case "1" : // Deposit
+				System.out.println("Deposit, please enter account number.");
+				account = userInput.nextLine();
+				System.out.println("Please enter your deposit amount:");
+				amount = userInput.nextDouble();
+				userInput.nextLine();
+				myBank.deposit(account, amount);
+				System.out.println(myBank.findAccount(account).getAccountType()
+						+ " account Balance is now $" 
+						+ myBank.findAccount(account).getBalance());
+				break;
+			case "2" : // Withdraw
+				System.out.println("Withdrawal, please enter account number.");
+				account = userInput.nextLine();
+				System.out.println("Please enter your withdrawal amount:");
+				amount = userInput.nextDouble();
+				userInput.nextLine();
+				myBank.withdraw(account, amount);
+				System.out.println(myBank.findAccount(account).getAccountType() 
+						+ " account Balance is now $" 
+						+ myBank.findAccount(account).getBalance());
+				break;
+			case "3" : // Check Balance
+				System.out.println("Which account would you like to check the balance on?");
+				account = userInput.nextLine();
+				System.out.println("You have $" + myBank.findAccount(account).getBalance()
+					+ " in " +myBank.findAccount(account).getAccountType());
+			case "4" : // Close Account
+				System.out.println("Which account would you like to close?");
+				account = userInput.nextLine();
+				BankAccount bankAccount = mybank.findAccount
+				myBank.close(account);
+				
+				System.out.println("You have $" + myBank.findAccount(account).getBalance()
+					+ " in " +myBank.findAccount(account).getAccountType());
+			case "5" : // Terminate app
+				System.out.println("Thank you for banking with us!");
+				System.exit(0);
+			default :
+				System.out.println("Please select from one of the options (1 - 5)");
+			} // end switch
+		
 		
 		} // End Menu Loop
 
-	}
-	public void Menu() {
-		
-	}
+	}  // End Main
+
+	public static void displayAccounts(Collection<BankAccount> collection) {
+		// iterate through bank accounts and display them
+		for (BankAccount currentAccount : collection) {
+			System.out.println("[" + currentAccount.getAccountNumber() + "]"
+					+ " " + currentAccount.getAccountType() + " $"
+					+ currentAccount.getBalance());
+		}
+	} // end displayAccounts();
+	
 } // end BankingApp();
